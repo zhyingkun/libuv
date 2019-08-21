c2cb3b49a00a0a01f22e178cb813a8765f129547
zhyingkun <1464959348@qq.com>
2019-08-21 11:18:40 +0800

    Print thread id in queue worker

--- examples/queue-work/main.c
+++ examples/queue-work/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include <uv.h>
 
-#define FIB_UNTIL 25
+#define FIB_UNTIL 12
 uv_loop_t *loop;
+pthread_t mainThread;
 
 long fib_(long t) {
     if (t == 0 || t == 1)
@@ -15,6 +17,8 @@ long fib_(long t) {
 }
 
 void fib(uv_work_t *req) {
+    pthread_t fibThread = pthread_self();
+    printf("fib: %p, equal main thread: %s\n", fibThread, pthread_equal(fibThread, mainThread)?"YES":"NO");
     int n = *(int *) req->data;
     if (random() % 2)
         sleep(1);
@@ -25,10 +29,15 @@ void fib(uv_work_t *req) {
 }
 
 void after_fib(uv_work_t *req, int status) {
+    (void)status;
+    pthread_t afterFibThread = pthread_self();
+    printf("after_fib: %p, equal main thread: %s\n", afterFibThread, pthread_equal(afterFibThread, mainThread)?"YES":"NO");
     fprintf(stderr, "Done calculating %dth fibonacci\n", *(int *) req->data);
 }
 
 int main() {
+    mainThread = pthread_self();
+    printf("main thread: %p\n", mainThread);
     loop = uv_default_loop();
 
     int data[FIB_UNTIL];
